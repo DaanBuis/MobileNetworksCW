3d7a90866e57f0fc4dc089d525d2b091664315bb
DaanBuis <dbuis201@caledonian.ac.uk>
2024-10-30 13:56:13 +0000

    Added Second Page with currently no functionality other than navigating between pages

--- app/src/main/java/org/me/gcu/coursework/ReviewPage.java
+++ app/src/main/java/org/me/gcu/coursework/ReviewPage.java
@@ -1,4 +1,12 @@
 package org.me.gcu.coursework;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
@@ -12,55 +20,40 @@ import android.widget.Button;
 import android.widget.Toast;
 import org.me.gcu.coursework.R;
 
-public class MainActivity extends AppCompatActivity {
-
-    private Button navReview;
-
+public class ReviewPage extends AppCompatActivity {
 
     @Override
-    protected void onCreate( Bundle savedInstanceState ) {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_review_page);
 
-        navReview = (Button)findViewById(R.id.reviewBH);
-        navReview.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                startActivity(new Intent(MainActivity.this,ReviewPage.class));
-            }
-        });
-        // calling this activity's function to
-        // use ActionBar utility methods
         ActionBar actionBar = getSupportActionBar();
 
-        // providing title for the ActionBar
-        actionBar.setTitle("");
+       actionBar.setTitle("");
 
 
 
         // methods to display the icon in the ActionBar
-        actionBar.setDisplayUseLogoEnabled(true);
-        actionBar.setDisplayShowHomeEnabled(true);
+       actionBar.setDisplayUseLogoEnabled(true);
+       actionBar.setDisplayShowHomeEnabled(true);
+
+
+
 
     }
-
-    // method to inflate the options menu when
-    // the user opens the menu for the first time
     @Override
     public boolean onCreateOptionsMenu( Menu menu ) {
 
         getMenuInflater().inflate(R.menu.main, menu);
         return super.onCreateOptionsMenu(menu);
     }
-
-    // methods to control the operations that will
-    // happen when user clicks on the action buttons
     @Override
     public boolean onOptionsItemSelected( @NonNull MenuItem item ) {
         if (item.getItemId() == R.id.settings) {
             Toast.makeText(this, "Settings Clicked", Toast.LENGTH_SHORT).show();
         } else if (item.getItemId() == R.id.backpage) {
-                Toast.makeText(this, "Backpage Clicked", Toast.LENGTH_SHORT).show();
+            finish();
         }
         return super.onOptionsItemSelected(item);
     }
